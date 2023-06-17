51168e9c32114e5438c1288d0a910e7971407259
nehanarta <nehabaswanthrao@gmail.com>
2023-06-17 08:45:56 +0530

    Added:first prog

---
+++ TwoStringEqual.java
@@ -0,0 +1,13 @@
+package com.bridglabz;
+
+public class TwoStringEqual {
+    public static void main(String[] args){
+        String str1="abc";
+        String str2="abc";
+        if(str1==str2){
+            System.out.println("str1 & str2 are equal");
+        }else{
+            System.out.println("str1 & str2 are not equal");
+        }
+    }
+}
